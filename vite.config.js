c4bd3fc4fbb5a77b95df052d94d17447798122ab
snyderdon20 <snyderdon20@gmail.com>
2026-05-19 10:34:30 -0600

    Unminified build for debugging

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: 'dist', sourcemap: false, minify: true },
+  define: { 'process.env.NODE_ENV': '"development"' },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: false,
+  },
 })
